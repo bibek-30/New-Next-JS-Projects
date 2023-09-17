b94fdb11caef98b8d9b903efc07c211f0d4201ff
Bibek_30 <93273348+bibek-30@users.noreply.github.com>
2023-09-17 13:50:09 +0545

    "Dark Mode"

--- src/components/Darkmode/darkmode.jsx
+++ src/components/Darkmode/darkmode.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import styles from "./darkmode.module.css";
+import { ThemeContext } from "../../../context/ThemeContext";
 
 const Darkmode = () => {
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icons}>🌛</div>
       <div className={styles.icons}>☀️</div>
-      <div className={styles.ball} />
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
     </div>
   );
 };
